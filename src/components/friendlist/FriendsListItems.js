7fb254e1a1a490200fbbf9b55ac4f848962b1a45
Kateryna Harmash <ekaterinagarmash1993@gmail.com>
2024-01-22 19:11:01 +0200

    delete problem

--- src/components/friendlist/FriendsListItems.js
+++
@@ -1,9 +0,0 @@
-import { FriendItem, FriendStatus, Avatar, Name } from "./FriendList.styled"
-
-export const FriendListItem = ({isOnline,avatar,name}) => {
-    return <FriendItem>
-            <FriendStatus $status={isOnline}></FriendStatus>
-            <Avatar src={avatar} alt={name} width="48" />
-            <Name>{name}</Name>
-        </FriendItem>
-}

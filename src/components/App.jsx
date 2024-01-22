1b478fd4fead6dd822d117182171ecc70362e623
Kateryna Harmash <139209869+KaterynaHarmash@users.noreply.github.com>
2024-01-22 09:00:00 +0200

    /

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from "./path/to/user.json";
-import data from './path/to/data.json';
-import friends from './path/to/friends.json';
-import transactions from './path/to/transactions.json';
+import user from "../path/to/user.json";
+import data from '../path/to/data.json';
+import friends from '../path/to/friends.json';
+import transactions from '../path/to/transactions.json';
 
 import { GlobalStyle } from "./GlobalStyle";
 
